2c5c067b2fa3dbf717d9da05fcac02c4383fe684
Calvin Tang <tcalvin9@gmail.com>
2021-12-11 22:52:07 -0500

    Changes to see if heroku would deploy

--- graphql/app.js
+++ graphql/app.js
@@ -1,7 +1,18 @@
 const Koa = require('koa')
+const serve = require('koa-static')
+const path = require('path')
 const { ApolloServer } = require('apollo-server-koa')
 const { makeExecutableSchema } = require('graphql-tools')
 const { resolvers, typeDefs } = require("./schemas")
+
+const app = new Koa()
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(serve('/src/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 4000
 
@@ -9,7 +20,6 @@ const server = new ApolloServer({
     schema: makeExecutableSchema({typeDefs, resolvers})
 })
 
-const app = new Koa()
 server.applyMiddleware({app})
 
 app.listen({port: PORT}, () => {
