174bf3fd3f8dc324d4b5266dde8757f75d6ed185
Calvin Tang <tcalvin9@gmail.com>
2021-12-12 01:09:53 -0500

    Update app.js

--- graphql/app.js
+++ graphql/app.js
@@ -18,7 +18,7 @@ const server = new ApolloServer({
 server.applyMiddleware({app})
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(serve('public'))
+    app.use(serve(path.join(__dirname, 'public')))
     router.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
     })
