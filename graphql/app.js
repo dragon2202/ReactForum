d6b05192800aa942591eae68f266fe5ca2e5c9c0
Calvin Tang <tcalvin9@gmail.com>
2021-12-12 00:03:31 -0500

    small changes to hopefully deploy

--- graphql/app.js
+++ graphql/app.js
@@ -1,9 +1,13 @@
 const Koa = require('koa')
+const serve = require('koa-static')
+const path = require('path')
+const Router = require('koa-router')
 const { ApolloServer } = require('apollo-server-koa')
 const { makeExecutableSchema } = require('graphql-tools')
 const { resolvers, typeDefs } = require("./schemas")
 
 const app = new Koa()
+const router = new Router()
 
 const PORT = process.env.PORT || 4000
 
@@ -13,6 +17,14 @@ const server = new ApolloServer({
 
 server.applyMiddleware({app})
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(serve('public'))
+    router.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+    })
+    app.use(router.routes()).use(router.allowedMethods())
+}
+
 app.listen({port: PORT}, () => {
     console.log(` Server ready at http://localhost:${PORT + server.graphqlPath}`)
-})
+}) 
