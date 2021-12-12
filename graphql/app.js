5b4df33d31cc8fcfc1742ab096f9019348b46f70
Calvin Tang <tcalvin9@gmail.com>
2021-12-11 23:20:12 -0500

    minor changes for heroku

--- graphql/app.js
+++ graphql/app.js
@@ -1,18 +1,9 @@
 const Koa = require('koa')
-const serve = require('koa-static')
-const path = require('path')
 const { ApolloServer } = require('apollo-server-koa')
 const { makeExecutableSchema } = require('graphql-tools')
 const { resolvers, typeDefs } = require("./schemas")
 
 const app = new Koa()
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(serve('/src/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'))
-    })
-}
 
 const PORT = process.env.PORT || 4000
 
